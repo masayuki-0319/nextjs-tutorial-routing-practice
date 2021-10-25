a88eb7e57d2c0e1aa64d38f79fdaf0d57fc105d4
将之大西 <omccello@gmail.com>
2021-10-25 09:51:09 +0900

    lec77

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,7 +1,14 @@
+import EventList from '../../components/events/event-list';
+import { getAllEvents } from '../../hooks/dummy-data';
+
 const AllEventsPage = () => {
+  const events = getAllEvents();
+  console.log(events);
+
   return (
     <div>
-      <h1>All Events</h1>
+      <h1>All Events Page</h1>
+      <EventList items={events} />
     </div>
   );
 };
