edf9d66b6f70a16ae3aaa99797e2d946aa06a489
将之大西 <omccello@gmail.com>
2021-10-26 23:17:07 +0900

    lec80

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,4 +1,36 @@
+import { useRouter } from 'next/dist/client/router';
+import { getFilteredEvents } from '../../hooks/dummy-data';
+
 const FilteredEventsPage = () => {
+  const router = useRouter();
+  const filterData = router.query.slug as [string, string];
+  console.log(filterData);
+
+  if (!filterData) {
+    return <p className='center'>Loading... </p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter.</p>;
+  }
+
+  const filteredEvent = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (!filteredEvent || filteredEvent.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
   return (
     <div>
       <h1>Filtered Events</h1>
